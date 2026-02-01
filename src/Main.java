d7609d089919eaed1f483c40fc08afb062ec02a5
Christian Xiong <Takachris9@gmail.com>
2026-02-01 17:47:12 -0600

    added a bit to Main and OrderHandler I might have oofed it }:

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,55 @@ public class Main {
 
         Debug debug = new Debug();
 
+
+
         // Maybe have to take in a file?
         Scanner scan = new Scanner(System.in);
         System.out.println("Could you give me the file path that you want? :D");
 
-        String fileName = scan.nextLine();
-        System.out.println("okok your file name is: " + fileName);
-        // Create Order_Interface Object and call menuChoice() and pass in fileName
+        String filePath = scan.nextLine();
+        System.out.println("okok your file path is: " + filePath);
 
-        // userInterface userinterface = new userInterface();
-        // userinterface.menuChoice();
+        // new json object = filename
 
-        //
+        //proxy method calls
+
+        // Create User_Interface Object and call menuChoice() and pass in fileName
+        UserInterface userInterface = new UserInterface();
+
+        // have parameters as strings so far,
+        // but if Jacob wants switch statements then maybe 1...n integers instead.
+        userInterface.menuChoice("loadFilePath", filePath); // Give user interface file path
+
+        userInterface.menuChoice("loadOrder"); // Load order
+
+        userInterface.menuChoice("displayOrder"); // Display Order
+
+        userInterface.menuChoice("startOrder"); // Start Order
+
+        userInterface.menuChoice("CompleteOrder"); // Complete Order
+
+        userInterface.menuChoice("displayUncomplete"); // Display Uncompleted Orders
+
+        userInterface.menuChoice("displayComplete"); //Display Completed Orders
+
+        userInterface.menuChoice("exportJSON"); // Export Json
+
+ //       JsonParser parser = new JsonParser();
+//        OrderHandler handler = new OrderHandler();
+//
+//        handler.doesOrderThings(parser.madeOrder());
+
+        //menuChoice(displayOrder) or displayStatus
+
+        //menuChoice(displayUncomplete)
+
+        //menuChoice(displayComplete)
+
+        //OrderHandler orderHandler = new OrderHandler(); // OrderHandler object
+        //accepts order from json parser
+        // accept json file use jsonmethods to create the order
+
+        // menuChoice(exportJSON)
     }
 }
--- src/OrderHandler.java
+++ src/OrderHandler.java
@@ -10,7 +10,7 @@ public class OrderHandler<E> {
     private LinkedList<E> completedOrders;
 
     //Using map to associate orders with an id per instruction 3
-    private Map<String,E> ordersById;
+    private Map<String, E> ordersById;
 
 
     //Constructor creates linked list depending on status and a map for associating orders with their ID
@@ -21,14 +21,17 @@ public class OrderHandler<E> {
         //used for look up.
         this.ordersById = new HashMap<>();
     }
+
     //getters for the linked lists
-    public LinkedList<E> getIncomingOrders(){
+    public LinkedList<E> getIncomingOrders() {
         return incomingOrders;
     }
-    public LinkedList<E> getStartedOrders(){
+
+    public LinkedList<E> getStartedOrders() {
         return startedOrders;
     }
-    public LinkedList<E> getCompletedOrders(){
+
+    public LinkedList<E> getCompletedOrders() {
         return completedOrders;
     }
 
@@ -38,54 +41,52 @@ public class OrderHandler<E> {
      */
     //loads all orders from parser into an incoming orders linkedList
     // need to figure out how to check for nulls and skip incase there is a gap in list.
-    public void loadOrders(List<E> parsedOrders){
-        for (E order : parsedOrders){
+    public void loadOrders(List<E> parsedOrders) {
+        for (E order : parsedOrders) {
             String id = order.getOrderId();
             incomingOrders.add(order);
-            ordersById.put(id,order);
-            order.setStatus("incoming");
+            ordersById.put(id, order);
+            order.getOrderStatus("incoming");
         }
     }
 
     // when prompted by user interface move specific incoming orders to started orders.
-    public void startOrder(String id){
-        if(ordersById.get(id) == null){
+    public void startOrder(String id) {
+        if (ordersById.get(id) == null) {
             System.out.println("No order associated with this id");
             return;
         }
-        if (ordersById.get(id).getStatus().equals("incoming")){
+        if (ordersById.get(id).getOrderStatus().equals("incoming")) {
             E order = ordersById.get(id);
-            order.setStatus("started");
+            order.setOrderStatus("started");
             startedOrders.add(order);
             incomingOrders.remove(order);
-        }
-        else{
+        } else {
             System.out.println("Can't start an order that has already been started or completed");
         }
 
     }
 
     // When prompted by user interface move started order to completed linkedlist
-    public void completeOrder(String id){
-        if(ordersById.get(id) == null){
+    public void completeOrder(String id) {
+        if (ordersById.get(id) == null) {
             System.out.println("No order associated with this id");
             return;
         }
-        if (ordersById.get(id).getStatus().equals("started")){
+        if (ordersById.get(id).getOrderStatus().equals("started")) {
             E order = ordersById.get(id);
-            order.setStatus("completed");
+            order.getOrderStatus("completed");
             completedOrders.add(order);
             startedOrders.remove(order);
-        }
-        else{
+        } else {
             System.out.println("Can't complete an order that hasn't been started yet.");
         }
     }
 
 
     //going to be used to grab an order by its order id using hashmap;
-    public E getOrder(String id){
-        if(ordersById.get(id) == null){
+    public E getOrder(String id) {
+        if (ordersById.get(id) == null) {
             System.out.println("No order associated with this id");
             return null;
         }
@@ -93,11 +94,59 @@ public class OrderHandler<E> {
     }
 
     //getting the total price of all orders that arent complete yet
-    public double totalPrice(HashMap<String,E> orders){
+    public double totalPrice(HashMap<String, E> orders) {
         //loop through hashmap
         // add total price from inside orders if status != complete
         //return price
     }
 
+    /*
+        Start of Chris edits oman
+        plz don't get angry Joey }: I'm trying my best
+
+     */
+    public E changeStatus(T order) {
+
+    }
+
+    public E displayOrder(T order) {
+
+    }
+
+    public void displayUncompletedOrders() {
+        // display incoming and started orders linkedlist
+        // Call order method for price
+        // getOrderPrice()
+        // price total
+        double totalPriceUncompletedOrders = 0;
+        System.out.println("Incoming Orders: ");
+        for(E Order : incomingOrders){
+            System.out.println(Order);
+            totalPriceUncompletedOrders += Order.getOrderPrice();
+        }
+
+        System.out.println("Started Orders: ");
+        for(E Order : startedOrders){
+            System.out.println(Order);
+            totalPriceUncompletedOrders += Order.getOrderPrice();
+        }
+    }
+
+    public E displayCompletedOrders() {
+        //display completedOrders linkedlist
+        System.out.println("Completed Orders: ");
+        for (E Order : completedOrders) {
+            System.out.println(Order);
+        }
+
+//        public E modifyOrder (T order){
+//
+//        }
+
+//    public E exportOrder(T order) {
+//
+//    }
+
+    }
 
 }
