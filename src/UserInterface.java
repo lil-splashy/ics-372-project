2699658417eb3c1606506cf32ff0c59e3320a032
jacobwiggins <jacjwiggins@aol.com>
2026-02-01 14:39:01 -0600

    Simple skeleton for the UI, no functionality yet.

---
+++ src/UserInterface.java
@@ -0,0 +1,13 @@
+/**
+ * This class handles the UI for order handling;
+ * @author Jacob Wiggins
+ */
+import java.util.Scanner;
+public class UserInterface
+{
+    public UserInterface()
+    {}
+
+    public static void main(String[] args)
+    {}
+}
