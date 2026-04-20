2a61d1cd368596aafe78f25e415f079cb96b9f25
Ben Wall <wall.benjamin@gmail.com>
2026-04-20 11:51:10 -0500

    adding extra functionality and breaking things into simpler objects

--- src/main/java/edu/UI/ConfirmationDialog.java
+++ src/main/java/edu/UI/ConfirmationDialog.java
@@ -8,21 +8,27 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.awt.*;
+import java.util.Objects;
+
 public class ConfirmationDialog extends Application {
     @Override
     public void start(Stage primaryStage) {
         // action that triggers the confirmation dialog
         primaryStage.setTitle("Confirmation Dialog");
         primaryStage.setScene(new Scene(new StackPane(), 300, 200));
+
         primaryStage.show();
         showConfirmationDialog();
     }
 
     private void showConfirmationDialog() {
-        Alert alert = new Alert(AlertType.CONFIRMATION);
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Confirmation");
         alert.setHeaderText("Are you sure you want to proceed?");
         alert.setContentText("Choose your option.");
+        String css = Objects.requireNonNull(this.getClass().getResource("resources/styles/alert.css")).toExternalForm();
+        alert.getDialogPane().getStylesheets().add(css);
 
         alert.showAndWait().ifPresent(response -> {
             if (response == ButtonType.OK) {
